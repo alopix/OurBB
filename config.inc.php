d0d27553a1173a54d06be1294516a7f493227f03
alopix <dustin.steiner@gmail.com>
2009-09-10 10:29:59 +0200

    a first standard-config-file

---
+++ config.inc.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * OurBB, a small but effective Burning Board
+ *
+ * Copyright (C) 2009 Dustin Steiner
+ *
+ * This work is licensed under the Creative Commons Attribution-Share Alike 3.0 Austria License.
+ * To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/3.0/at/ or send a letter to
+ *  Creative Commons, 171 Second Street, Suite 300, San Francisco, California, 94105, USA.
+ */
+
+// the hostname of the mysql-server
+$sqlHost = "localhost";
+// the username and password to log into your database
+$sqlUser = "foe";
+$sqlPassword = "bar";
+// the database where to find the OurBB-tables
+$sqlDB = "ourbb";
+
+// the prefix to se for OurBB-tables
+$dbPrefix = "ourbb_";
+
+// the admin-mail to send errors to
+$adminmail = "foe@bar.net";
+?>
